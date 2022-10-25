b346c1ea24bc78cacc9ae16fea999802d458549b
ryannas <ryannas@163.com>
2022-10-25 17:38:50 +0800

    add ray.h

---
+++ include/ray.h
@@ -0,0 +1,21 @@
+#ifndef RAY_H
+#define RAY_H
+
+#include "vec.h"
+
+class Ray{
+public:
+    Vec3 origin;
+    Vec3 dir;
+
+    Ray() {};
+    Ray(const Vec3& o, const Vec3& d)
+        :origin(o), dir(d) {
+    }
+
+    Vec3 at(double t) {
+        return origin + t * dir;
+    }
+};
+
+#endif
