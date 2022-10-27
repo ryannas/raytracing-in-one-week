002bfc555bcd832f9eb509c08e62fd6e97e70f00
ryannas <ryannas@163.com>
2022-10-27 15:29:15 +0800

    add camera abstraction

---
+++ include/camera.h
@@ -0,0 +1,30 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include "rtweekend.h"
+
+class Camera {
+public:
+    Camera() {
+        auto aspect_ratio = 16.0 / 9.0;
+        auto viewport_height = 2.0;
+        auto viewport_width = aspect_ratio * viewport_height;
+        auto focal_length = 1.0;
+
+        origin = point3(0, 0, 0);
+        horizontal = Vec3(viewport_width, 0.0, 0.0);
+        vertical = Vec3(0.0, viewport_height, 0.0);
+        lower_left_corner = origin - horizontal / 2 - vertical / 2 - Vec3(0.0, 0.0, focal_length);
+    }
+    
+    Ray getRay(double u, double v) {
+        return Ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+    }
+private:
+    point3 origin;
+    point3 lower_left_corner;
+    Vec3 horizontal;
+    Vec3 vertical;
+};
+
+#endif
