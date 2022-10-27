0913e1ef7a9db8c857afce8aa4a7e8dd01f6cae6
ryannas <ryannas@163.com>
2022-10-27 15:42:07 +0800

    add antialiasing via super-sampling

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include "../include/hittable_list.h"
 #include "../include/sphere.h"
 
+#include "../include/camera.h"
+
 #include <iostream>
 
 color ray_color(const Ray& r, const HittableList& world) {
@@ -19,30 +21,27 @@ int main() {
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = image_width / aspect_ratio;
-
-    auto viewport_height = 2.0;
-    auto viewport_width = viewport_height * aspect_ratio;
-    auto focal_length = 1.0;
-
-    auto origin = Vec3(0, 0, 0);
-    auto horizontal = Vec3(viewport_width, 0, 0);
-    auto vertical = Vec3(0, viewport_height, 0);
-    auto lower_left_corner = origin - horizontal/2 - vertical/2 - Vec3(0, 0, focal_length);
+    const int samples_per_pixel = 100;
 
     HittableList world;
     world.add(make_shared<Sphere>(point3(0, 0, -1), 0.5));
     world.add(make_shared<Sphere>(point3(0, -100.5, -1), 100));
+
+    Camera camera;
 
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
     for (int j = image_height - 1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << " " << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            auto u = double(i) / (image_width - 1);
-            auto v = double(j) / (image_height - 1);
-            Ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-            color pixel_color = ray_color(r, world);
-            write_color(std::cout, pixel_color);
+            color pixel_color(0, 0, 0);
+            for (int s = 0; s < samples_per_pixel; ++s) {
+                auto u = (i + random_double()) / (image_width - 1);
+                auto v = (j + random_double()) / (image_height - 1);
+                Ray r = camera.getRay(u, v);
+                pixel_color += ray_color(r, world);
+            }
+            write_color(std::cout, pixel_color, samples_per_pixel);
         }
     }
 
